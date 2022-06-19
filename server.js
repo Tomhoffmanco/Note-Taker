2e8a86febd63f8c6847d04443ee54def9d89e284
Tom hoffman <tomhoffmanco@gmail.com>
2022-06-19 17:10:52 -0400

    refractored code

--- server.js
+++ server.js
@@ -1,61 +1,53 @@
-// dependencies
-
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
-const util = require("util");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
 
-//handling asynchronous process
-const readFileAsync = util.promisify(fs.readFileSync);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-// set up server
+// setting up server
 const app = express();
-const PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3001;
 
-// static middleware
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-// api route / get request
-app.get("/api/notes", function (req, res) {
-  readFileAsync("./db/db.json", "utf-8").then(function (data) {
-    notes = [].concat(JSON.parse(data));
-    res.join(notes);
-  });
+//Setting routes for APIs
+//get request
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-// api route / post request
-app.post("/api/notes", function (req, res) {
-  const note = req.body;
-  readFileAsync("./db/db.json", "utf-8")
-    .then(function (data) {
-      const notes = [].concat(JSON.parse(data));
-      note.id = notes.length + 1;
-      notes.push(note);
-      return notes;
-    })
-    .then(function (notes) {
-      writeFileAsync("./db/db.json", JSON.stringify(notes));
-      res.JSON(note);
-    });
+// Post rooute/ request
+app.post("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const newNotes = req.body;
+  newNotes.id = uuid.v4();
+  notes.push(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  res.json(notes);
 });
 
-// api route / delete request
-app.delete();
+//delete request
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+  res.json(delNote);
+});
 
-//  html routes
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//call for notes.html
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get();
-res.sendFile(path.join(__dirname, "./public/index.html"));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
+//Start listen
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
