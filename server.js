590adbc5cd38c10f62fe36d45ca1200e0000f352
Tom hoffman <tomhoffmanco@gmail.com>
2022-06-19 16:02:50 -0400

    create post & get request

--- server.js
+++ server.js
@@ -18,10 +18,28 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 
 // api route / get request
-app.get();
+app.get("/api/notes", function (req, res) {
+  readFileAsync("./db/db.json", "utf-8").then(function (data) {
+    notes = [].concat(JSON.parse(data));
+    res.join(notes);
+  });
+});
 
 // api route / post request
-app.post();
+app.post("/api/notes", function (req, res) {
+  const note = req.body;
+  readFileAsync("./db/db.json", "utf-8")
+    .then(function (data) {
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1;
+      notes.push(note);
+      return notes;
+    })
+    .then(function (notes) {
+      writeFileAsync("./db/db.json", JSON.stringify(notes));
+      res.JSON(note);
+    });
+});
 
 // api route / delete request
 app.delete();
@@ -33,3 +51,11 @@ app.get("/notes", function (req, res) {
 
 app.get();
 res.sendFile(path.join(__dirname, "./public/index.html"));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
